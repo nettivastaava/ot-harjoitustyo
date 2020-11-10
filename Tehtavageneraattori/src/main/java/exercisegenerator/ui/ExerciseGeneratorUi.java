76c3ebfdfadaaac17b0e06c0ac6d0f6e4a97dfa9
nettivastaava <vaxandst@cubbli20-gold.local>
2020-11-10 15:55:07 +0200

    testi

--- Tehtavageneraattori/src/main/java/exercisegenerator/ui/ExerciseGeneratorUi.java
+++ Tehtavageneraattori/src/main/java/exercisegenerator/ui/ExerciseGeneratorUi.java
@@ -5,31 +5,66 @@
  */
 package exercisegenerator.ui;
 
+import java.util.Properties;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 
 /**
  *
  * @author vaxandst
  */
 public class ExerciseGeneratorUi extends Application {
-    private Scene login;
-    private Scene register;
-    private Scene exerciseList;
-    private Scene createExercise;
-    private Scene solveExercise;
+    private Scene loginScene;
+    private Scene registerScene;
+    private Scene exercisesScene;
+    private Scene createExerciseScene;
+    private Scene solveExerciseScene;
+    
+   
     
     @Override
-    public void start(Stage window) throws Exception {
-      
-        window.show();
-    }
+public void start(Stage ikkuna) {
+
+    Label nimiTeksti = new Label("Nimi: ");
+    TextField nimiKentta = new TextField();
+    Label hetuTeksti = new Label("Hetu: ");
+    TextField hetuKentta = new TextField();
+
+    Button lisaaNappi = new Button("Lisää henkilö!");
+
+    GridPane komponenttiryhma = new GridPane();
+    komponenttiryhma.add(nimiTeksti, 0, 0);
+    komponenttiryhma.add(nimiKentta, 1, 0);
+    komponenttiryhma.add(hetuTeksti, 0, 1);
+    komponenttiryhma.add(hetuKentta, 1, 1);
+    komponenttiryhma.add(lisaaNappi, 1, 2);
+
+    // tyylittelyä: lisätään tyhjää tilaa reunoille ym
+    komponenttiryhma.setHgap(10);
+    komponenttiryhma.setVgap(10);
+    komponenttiryhma.setPadding(new Insets(10, 10, 10, 10));
+
+    Scene nakyma = new Scene(komponenttiryhma);
+
+    ikkuna.setScene(nakyma);
+    ikkuna.show();
+}
     
     public static void main(String[] args) {
         launch(args);
