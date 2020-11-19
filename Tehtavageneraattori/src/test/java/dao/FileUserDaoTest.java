7be5b47a1d7ab838200e8f6ff90dfefa1c5c93c4
nettivastaava <vaxandst@cubbli20-gold.local>
2020-11-19 12:05:17 +0200

    FileUserDao tests

---
+++ Tehtavageneraattori/src/test/java/dao/FileUserDaoTest.java
@@ -0,0 +1,60 @@
+package dao;
+
+import exercisegenerator.dao.FileUserDao;
+import exercisegenerator.dao.UserDao;
+import exercisegenerator.domain.User;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import static org.junit.Assert.*;
+
+
+public class FileUserDaoTest {
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+    
+    File userFile;  
+    UserDao userDao;
+    
+    @Before
+    public void setUp() throws Exception {
+        userFile = testFolder.newFile("testfile_users.txt");  
+        
+        try (FileWriter file = new FileWriter(userFile.getAbsolutePath())) {
+            file.write("Heimo;huima\n");
+        }
+        
+        userDao = new FileUserDao(userFile.getAbsolutePath());
+    }
+    
+    @Test
+    public void usersAreReadCorrectlyFromFile() {
+        List<User> users = userDao.getAll();
+        assertEquals(1, users.size());
+        User user = users.get(0);
+        assertEquals("Heimo", user.getUsername());
+        assertEquals("huima", user.getPassword());
+    }
+   
+    @Test
+    public void userIsFound() {
+        User user = userDao.findByUsername("Heimo");
+        assertEquals("Heimo", user.getUsername());
+    }
+    
+    @Test
+    public void nonExistingUserIsNotFound() {
+        User user = userDao.findByUsername("Jallu");
+        assertEquals(null, user);
+    }
+    
+    @After
+    public void tearDown() {
+        userFile.delete();
+    }
+}
