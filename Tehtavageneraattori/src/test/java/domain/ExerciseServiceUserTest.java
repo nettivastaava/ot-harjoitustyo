69d20f115270333823a6c0c8104efcbbf88497ef
nettivastaava <vaxandst@cubbli20-gold.local>
2020-11-19 11:38:00 +0200

    login tests

--- Tehtavageneraattori/src/test/java/domain/ExerciseServiceUserTest.java
+++ Tehtavageneraattori/src/test/java/domain/ExerciseServiceUserTest.java
@@ -42,4 +42,28 @@ public class ExerciseServiceUserTest {
         boolean result = exService.createUser(testUser);
         assertFalse(result);
     }
+    
+    @Test
+    public void loginFailsWithWrongPassword() throws Exception {
+        boolean result = exService.login("Heimo", "salasana");
+        assertFalse(result);
+        
+        assertEquals(null, exService.getLoggedUser());
+    }
+    
+    @Test
+    public void loginSucceedsWithCorrectCredentials() throws Exception {
+        boolean result = exService.login("Heimo", "kissa");
+        assertTrue(result);       
+        
+        assertNotEquals(null, exService.getLoggedUser());
+    }
+    
+    @Test
+    public void userCanLogout() {
+        exService.login("Heimo", "kissa");
+        exService.logout();
+        
+        assertEquals(null, exService.getLoggedUser());
+    }    
 }
