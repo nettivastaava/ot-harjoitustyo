9166b3680b8adf9f3be74e1832186fc58472c6ed
nettivastaava <vaxandst@cubbli20-gold.local>
2020-11-30 14:17:05 +0200

    Style checked

--- Tehtavageneraattori/src/main/java/exercisegenerator/domain/ExerciseService.java
+++ Tehtavageneraattori/src/main/java/exercisegenerator/domain/ExerciseService.java
@@ -19,7 +19,7 @@ public class ExerciseService {
     }
     
     public boolean createUser(User user)  {   
-        if (userDao.findByUsername(user.getUsername()) != null || user.isValid()==false) {
+        if (userDao.findByUsername(user.getUsername()) != null || user.isValid() == false) {
             return false;
         }
         
--- Tehtavageneraattori/src/main/java/exercisegenerator/domain/User.java
+++ Tehtavageneraattori/src/main/java/exercisegenerator/domain/User.java
@@ -10,8 +10,8 @@ public class User {
 
     
     public User(String username, String password) {
-        this.username=username;
-        this.password=password;
+        this.username = username;
+        this.password = password;
 
     }
     
